781f1cefa9a1a97a148ecd2a41cace5d4db77bae
Alexey <brech_36@mail.ru>
2023-01-18 22:33:11 +0900

    add double

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x%2==0)
+{
+    Console.WriteLine($"{x} кратное число");
+}
+if (x%2!=0)
+{
+     Console.WriteLine($"{x} не кратное число");
+}
+
